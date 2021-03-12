65e279e8d2f58b99ef9d66c9c22019ec0ab467e5
Ellezique <gizellevanzyl@gmail.com>
2021-03-12 15:07:29 +1000

    added profiles route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :profiles
   devise_for :users
   root to: "home#page"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # http://localhost:3000/profiles
+  get "/", to: "profiles#index"
+
 end

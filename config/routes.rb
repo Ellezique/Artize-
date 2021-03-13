738b137c9e6fdc085393ab209d3f9fd3f4c95f7c
Ellezique <gizellevanzyl@gmail.com>
2021-03-13 18:34:27 +1000

    Generate scaffold style and migrate database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :styles
   resources :artists
   resources :orderitems
   resources :orders
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_075958) do
+ActiveRecord::Schema.define(version: 2021_03_13_083317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,12 @@ ActiveRecord::Schema.define(version: 2021_03_13_075958) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "styles", force: :cascade do |t|
+    t.string "style_description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|

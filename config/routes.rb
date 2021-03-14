7660ed34e987f3caff81c374a547bc363e6284d3
Ellezique <gizellevanzyl@gmail.com>
2021-03-14 15:58:28 +1000

    add links to buttons and navbar, add home page image hover transition

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,16 @@ Rails.application.routes.draw do
   resources :customers
   resources :profiles
   devise_for :users
+  
   # http://localhost:3000
   root to: "home#page"
+
+
+
   # http://localhost:3000/profiles   MUST BE RESTRICTED TO ADMIN AND USER CREATING IT.
-  get "/", to: "profiles#index"
+ # get "/", to: "profiles#index"
   # http://localhost:3000/artworks  MUST BE RESTRICTED TO ADMIN AND USER CREATING IT.
-  get "/", to: "artworks#index"
+  #get "/", to: "artworks#index"
  # http://localhost:3000/artists 
-  get "/", to: "artist#index"
+ # get "/", to: "artist#index"
 end

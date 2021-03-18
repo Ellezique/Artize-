32341880dcac8c5513e6066dbd5dadc6d18ca448
Ellezique <gizellevanzyl@gmail.com>
2021-03-18 12:59:39 +1000

    Populate seed.rb, update navbar and link buttons

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,68 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_033700) do
+ActiveRecord::Schema.define(version: 2021_03_18_021727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "artist_first_name"
+    t.string "artist_last_name"
+    t.text "about_artist"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "artmedia", force: :cascade do |t|
+    t.string "artmedium_description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "artworks", force: :cascade do |t|
+    t.string "art_title"
+    t.text "art_description"
+    t.boolean "available"
+    t.bigint "artist_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artist_id"], name: "index_artworks_on_artist_id"
+  end
+
+  create_table "styles", force: :cascade do |t|
+    t.string "style_description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +85,7 @@ ActiveRecord::Schema.define(version: 2021_03_11_033700) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "artworks", "artists"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #clean up rails db:reset rails db:seed rails db:migrate
-ArtworksArtmedium.destroy_all
+#ArtworksArtmedium.destroy_all
 #ArtworksOrder.destroy_all
-ArtworksStyle.destroy_all
+#ArtworksStyle.destroy_all
 Style.destroy_all
 Artmedium.destroy_all
 #Order.destroy_all
-#Artwork.destroy_all    #depends on artist
-#Artist.destroy_all     #depends on user>profile
+Artwork.destroy_all    
+Artist.destroy_all     
 
-#ARTISTS
-#testtest = Artist.create(about_artist: "Test is a test artist.", profile_id: 1)
-#leon = Artist.create(about_artist: "Leon Devenice is an award-winning [...] artist painter whose unique work portrays nature’s majestic beauty. His art often depicts the glorious richness of alfresco living and is lauded for its sense of romance and passion. Unafraid to incorporate bold colors, Leon speaks the language of vitality and sensuality with his incredible landscapes and painted scenes.", profile_id: )
-#gizelle = Artist.create(about_artist: "Gizelle v.Z is a mysterious artist who works with various media.", profile_id: )
 
 #STYLE AND MEDIA
 objective = Style.create(style_description: "Objective")
@@ -27,7 +23,7 @@ abstract = Style.create(style_description: "Abstract")
 subjective = Style.create(style_description: "Subjective")
 figurative = Style.create(style_description: "Figurative")
 geometric = Style.create(style_description: "Geometric")
-photorealism = Style.create(style_description: "Photorealism")
+realism = Style.create(style_description: "Realism")
 pop = Style.create(style_description: "Pop")
 illustration = Style.create(style_description: "Illustration")
 portait = Style.create(style_description: "Portrait")
@@ -58,6 +54,27 @@ glass = Artmedium.create(artmedium_description: "Glass")
 clay = Artmedium.create(artmedium_description: "Clay")
 ceramic = Artmedium.create(artmedium_description: "Ceramic")
 
-#ARTWORKS
-#leon.artworks.create(art_title: "Still life with Fruits & Wine", art_description: "Oil painting on canvas", art_price: 750, available: true, styles: landscape, media: [painting, oil_paint]) 
-#gizelle.artworks.create(art_title: "Weathered Boat", art_description: "Black and White photograph of a weather damaged boat.", art_price: 250, available: true, styles: black_and_white, media: photograph)
+#ARTISTS & their artworks
+leon = Artist.create(artist_first_name: "Leon", artist_last_name: "Devenice", about_artist: "Leon Devenice is an award-winning artist painter whose unique work portrays nature’s majestic beauty. His art often depicts the glorious richness of alfresco living and is lauded for its sense of romance and passion. Unafraid to incorporate bold colors, Leon speaks the language of vitality and sensuality with his incredible landscapes and painted scenes.")
+leon.artworks.create(art_title: "Still Life with Fruits & Wine", art_description: "Colorful fruit and wine", available: true)
+leon.artworks.create(art_title: "Glowing Crimson", art_description: "Colorful autumn trees", available: true)
+leon.artworks.create(art_title: "Sunset Embrace", art_description: "Colorful ocean waves", available: true)
+leon.artworks.create(art_title: "Voyages of Imagination", art_description: "Ship embarked on journey", available: true)
+
+gizelle = Artist.create(artist_first_name: "Gizelle", artist_last_name: "v.Z",about_artist: "Gizelle v.Z is a mysterious artist who works with various media.")
+gizelle.artworks.create(art_title: "Weathered Boat", art_description: "Black and white weathered boat", available: true)
+gizelle.artworks.create(art_title: "Wreckage", art_description: "Wrecked remains of a boat", available: true)
+
+toko = Artist.create(artist_first_name: "Toko", artist_last_name: "Suzuki", about_artist: "Toko Suzuki is a digital artist and skilled at drawing. Japanese born in NY, currently based in Sydney, Australia. Mostly working digitally and sometimes traditionally.")
+toko.artworks.create(art_title: "Backlight", art_description: "Faded image capturing the light", available: true)
+toko.artworks.create(art_title: "Real Time", art_description: "After Jeremy Mann", available: true)
+toko.artworks.create(art_title: "WangjieLi", art_description: "After Wangjie Li", available: true)
+
+steve = Artist.create(artist_first_name: "Steve", artist_last_name: "Johnson", about_artist: "Abstract artist.")
+steve.artworks.create(art_title: "No. 1", art_description: "First in series", available: true)
+steve.artworks.create(art_title: "No. 2", art_description: "Second in series", available: true)
+steve.artworks.create(art_title: "No. 3", art_description: "Third in series", available: true)
+steve.artworks.create(art_title: "No. 4", art_description: "Fourth in series", available: true)
+steve.artworks.create(art_title: "No. 5", art_description: "Fifth in series", available: true)
+steve.artworks.create(art_title: "No. 6", art_description: "Sixth in series", available: true)
+steve.artworks.create(art_title: "Droplet", art_description: "Mosaic droplet", available: true)

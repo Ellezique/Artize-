40c8d093c6cfefdf2faa5fda2c589cf4a2446783
Ellezique <gizellevanzyl@gmail.com>
2021-03-21 22:20:49 +1000

    New artwork adds styles and media. added accepts_nested_attributes_for :styles, :media to artworks model. Artworks controller update prams to includ ids for styles and artmedia.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -6,7 +6,7 @@ class Artist < ApplicationRecord
     return "#{artist_first_name} #{artist_last_name}"
   end 
   
- # def self.find_by_name(artist_first_name, artist_last_name)
- #   return Author.find_by(artist_first_name: artist_first_name, artist_last_name: artist_last_name)
- # end  
+  #def self.find_by_name(artist_first_name, artist_last_name)
+  #  return Author.find_by(artist_first_name: artist_first_name, artist_last_name: artist_last_name)
+  #end  
 end
--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -5,7 +5,7 @@ class Artwork < ApplicationRecord
   has_many :artworks_artmedia, dependent: :destroy
   has_many :artmedia, through: :artworks_artmedia
 
-  accepts_nested_attributes_for :styles, :artmedia
+  accepts_nested_attributes_for :styles, :artmedia 
 
   def list_styles
     (self.styles.map { |style| style.style }).join(", ")

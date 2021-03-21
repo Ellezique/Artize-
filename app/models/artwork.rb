28d183335c271f3f7b44e647dc03c7d17d133401
Ellezique <gizellevanzyl@gmail.com>
2021-03-21 17:36:50 +1000

    Artwork helper method to show styles and artmediums. Update artwork views to show Artwork's styles and artmediums

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,8 +1,15 @@
 class Artwork < ApplicationRecord
   belongs_to :artist
-  has_many :artworks_styles
+  has_many :artworks_styles, dependent: :destroy
   has_many :styles, through: :artworks_styles
-  has_many :artworks_artmedia
+  has_many :artworks_artmedia, dependent: :destroy
   has_many :artmedia, through: :artworks_artmedia
 
+  def list_styles
+    (self.styles.map { |style| style.style }).join(", ")
+  end
+
+  def list_artmedia
+    (self.artmedia.map { |artmedium| artmedium.artmedium }).join(", ")
+  end
 end

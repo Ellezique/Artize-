5ee8b8d4da96d4e3220ff5e4fe1895dd29fd6451
Ellezique <gizellevanzyl@gmail.com>
2021-03-24 00:17:59 +1000

    Stripe frontend. Checkout not working without refresh.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,13 +4,13 @@ class OrdersController < ApplicationController
 
     Stripe.api_key = ENV['STRIPE_SK_TEST']
     @session = Stripe::Checkout::Session.create(
-      payment_method_types: ['card'],
+      payment_method_types: ['card'], 
       success_url: order_success_url,
-      cancel_url: order_cancel_url,
+      cancel_url: order_cancel_url, 
       line_items: [
         {
           name: @artwork.art_title,
-          description: "by #{@artwork.artist.artist_name}"
+          description: "by #{@artwork.artist.artist_name}",
           images: @artwork.artimage.attached? ? [@artwork.artimage.service_url] : [],
           amount: (@artwork.price * 100).to_i,
           currency: 'aud',
@@ -19,12 +19,12 @@ class OrdersController < ApplicationController
       ]
     )
   end
-
+  
   def success
-    render html: "<h4>Thank you for your order.<h4>"
+    render html: "<h4>Thanks for your order!</h4>".html_safe
   end
 
   def cancel
-    render html: "<h4>The order was cancelled.<h4>"
+    render html: "<h4>The order was cancelled.<h4>".html_safe
   end
 end
